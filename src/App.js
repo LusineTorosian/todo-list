76e0fdf078d1782c09c765871bdeca08590c87c9
unknown <lusinetorosyan.evistep@gmail.com>
2022-10-02 21:27:40 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import TodoList from './TodoList';
+import ToDoForm from './ToDoForm';
+import ToDoFooter from './ToDoFooter';
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: Math.random(),
+      text: "Learn JS",
+      isCompleted: false
+    },
+    {
+      id: Math.random(),
+      text: "Learn CSS",
+      isCompleted: false
+    },
+    {
+      id: Math.random(),
+      text: "Learn React",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <ToDoForm onAdd={(text) => {
+        setTodos([
+          ...todos,
+          {
+            id: Math.random(),
+            text: text,
+            isCompleted: false
+          }
+        ]);
+      }} />
+      <TodoList
+        todos={todos}
+        onDelete={(todo) => {
+          setTodos(todos.filter((t) => t.id !== todo.id));
+        }}
+        onChange={(newTodo) => {
+          setTodos(todos.map((todo) => {
+            if (todo.id === newTodo.id) {
+              return newTodo;
+            }
+            return todo;
+          }));
+        }}
+      />
+      <ToDoFooter todos={todos} onClearCompleted={() => {
+        setTodos(todos.filter((todo) => !todo.isCompleted));
+      }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ToDoFooter.js
@@ -0,0 +1,12 @@
+function ToDoFooter({ todos, onClearCompleted }) {
+
+    const completedSize = todos.filter((todo) => todo.isCompleted).length;
+    return (
+        <div>
+            <span>{completedSize}/{todos.length} Completed</span>
+            <button onClick={onClearCompleted}>Clear Completed</button>
+        </div>
+    )
+}
+
+export default ToDoFooter; 
